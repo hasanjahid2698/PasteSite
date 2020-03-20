58e31a71fdd54787d334d00e9636a55a21721a5f
hasanjahid2698 <hasanjahid2698@gmail.com>
2020-03-20 12:15:09 +0600

    commit changing create and listview update and delete view

--- blog/urls.py
+++ blog/urls.py
@@ -1,6 +1,17 @@
 from django.urls import path
+from .views import (
+    PostListView,
+    PostDetailView,
+    PostCreateView,
+    PostUpdateView,
+    PostDeleteView
+)
 from . import views
 
 urlpatterns = [
-    path('', views.home, name='blog-Home'),
+    path('', PostListView.as_view(), name='blog-Home'),
+    path('post/<int:pk>/', PostDetailView.as_view(), name='post-detail'),
+    path('post/<int:pk>/update/', PostUpdateView.as_view(), name='post-update'),
+    path('post/<int:pk>/delete/', PostDeleteView.as_view(), name='post-delete'),
+    path('post/new/', PostCreateView.as_view(), name='post-create'),
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,4 +1,12 @@
 from django.shortcuts import render
+from django.contrib.auth.mixins import LoginRequiredMixin,UserPassesTestMixin
+from django.views.generic import (
+    ListView,
+    DetailView,
+    CreateView,
+    UpdateView,
+    DeleteView
+)
 from .models import PostText
 
 # Create your views here.
@@ -8,3 +16,48 @@ def home(request):
     }
     return render(request, 'blog/home.html',context)
 
+
+class PostListView(ListView):
+    model = PostText
+    template_name = 'blog/home.html'    #<app>/<model>_<viewtype>.html
+    context_object_name = 'posts'
+    ordering = ['-date_posted']
+
+
+class PostDetailView(DetailView):
+    model = PostText
+
+
+class PostCreateView(LoginRequiredMixin,CreateView):
+    model = PostText
+    fields  = ['title','expiration', 'content']
+
+    def form_valid(self, form):
+        form.instance.author = self.request.user
+        return super().form_valid(form)
+
+
+class PostUpdateView(LoginRequiredMixin,UserPassesTestMixin ,UpdateView):
+    model = PostText
+    fields  = ['title','expiration', 'content']
+
+    def form_valid(self, form):
+        form.instance.author = self.request.user
+        return super().form_valid(form)
+
+    def test_func(self):
+        post = self.get_object()
+        if self.request.user == post.author:
+            return True
+        return False
+
+class PostDeleteView(LoginRequiredMixin,UserPassesTestMixin ,DeleteView):
+    model = PostText
+
+    def test_func(self):
+        post = self.get_object()
+        if self.request.user == post.author:
+            return True
+        return False
+
+    
